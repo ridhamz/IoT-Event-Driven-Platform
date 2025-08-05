23917b9b2e2ea27a66f8cecd50439df148c74337
ridhamezrigui <ridhamezrigui07@gmail.com>
2025-08-05 13:24:16 +0100

    Fixed user creation

--- commands/user.go
+++ commands/user.go
@@ -35,3 +35,22 @@ func HandleLoginUser(req domain.LoginRequest) (string, error) {
 	// Publish event
 	//return events.PublishUserCreated(id, name)
 }
+
+func HandleCreateUser(user domain.User) error {
+	db := infrastructure.GetDB()
+	query := `
+        INSERT INTO users (first_name, last_name, email, password, created_at)
+        VALUES (?, ?, ?, ?, ?)
+    `
+	_, err := db.Exec(query,
+		user.FirstName,
+		user.LastName,
+		user.Email,
+		user.Password,
+		user.CreatedAt,
+	)
+	return err
+
+	// Publish event
+	//return events.PublishUserCreated(id, name)
+}
