c281d3283537fa3fdf6959bf2182fc5d6208f36f
ridhamezrigui <ridhamezrigui07@gmail.com>
2025-08-06 12:24:51 +0100

    Added .gitignore file

--- logger/logger.go
+++ logger/logger.go
@@ -1,10 +1,25 @@
 package logger
 
-import "github.com/sirupsen/logrus"
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
 
 var Log = logrus.New()
 
 func Init() {
+	// Open or create the log file
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		// Fall back to stderr if file can't be opened
+		logrus.Fatalf("Failed to open log file: %v", err)
+	}
+
+	// Set output to the file
+	Log.SetOutput(file)
+
+	// Set formatter and level
 	Log.SetFormatter(&logrus.JSONFormatter{}) // or use TextFormatter
 	Log.SetLevel(logrus.DebugLevel)
 }
