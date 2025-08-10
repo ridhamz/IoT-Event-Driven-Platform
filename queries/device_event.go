62108a23c7c2f8aaf816ab399018abe358587d8c
ridhamezrigui <ridhamezrigui07@gmail.com>
2025-08-10 17:26:03 +0100

    Added read config from the .env file

--- queries/device_event.go
+++ queries/device_event.go
@@ -6,7 +6,6 @@ import (
 )
 
 func ProcessDeviceEvent(event domain.DeviceEvent) error {
-	// Your logic here, e.g., insert event into DB
 	fmt.Printf("Processing device event: %+v\n", event)
 	return nil
 }
